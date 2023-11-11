b4df462599d23e926564f7b934030a7fd06bed1f
Olga-Gromova <gromova.o.77@gmail.com>
2023-11-11 15:41:47 +0100

    For checking

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+    private signature: number;
+    
+    constructor() {
+        this.signature = Math.random();
+    } 
 
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('The door was opened. The person come in the house.');            
+        } else {
+            console.log('The door is closed. Please, open the door.');            
+        }
+    }   
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+            console.log('The door is opened.');
+        } else {
+            console.log('The key is incorrect. The door is closed.');            
+        }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
